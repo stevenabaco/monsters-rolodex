91375b86621394c4f2f8ff4266177f36e5499e5a
Steven Abaco <steve@wizardrywd.com>
2020-08-12 17:08:22 -0500

    Updates

---
+++ precache-manifest.9c330418f78c0b0e7130e826903b2eb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179c39cb81c736f082f33a20b453569f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8fbc07749a5af640762a",
+    "url": "/monsters-rolodex/static/css/main.ece91cdf.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fbc07749a5af640762a",
+    "url": "/monsters-rolodex/static/js/main.bf1a6c2f.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
